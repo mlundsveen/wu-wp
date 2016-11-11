819357176a540197784335b4638835bf44b3710e
Michael A. Lundsveen <michael@lundsveen.no>
2016-11-11 14:33:44 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+
+add_image_size( 'nei', 800, 900, true ); //300 pixels
+
+
+/**
+ * Register our sidebars and widgetized areas.
+ *
+ */
+function arphabet_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'Top',
+		'id'            => 'top_widget',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+  register_sidebar( array(
+		'name'          => 'hmmmm',
+		'id'            => 'damn_widget',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="rounded">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', 'arphabet_widgets_init' );
